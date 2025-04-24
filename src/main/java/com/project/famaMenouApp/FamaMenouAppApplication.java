82b1f82a5500f95af77a0d65a846e94fab32125a
mohamedkhammeri <mohamed.khammeri@esprit.tn>
2025-04-24 16:40:04 +0100

    Initial commit

---
+++ src/main/java/com/project/famaMenouApp/FamaMenouAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.famaMenouApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamaMenouAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamaMenouAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/famaMenouApp/FamaMenouAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.famaMenouApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamaMenouAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
